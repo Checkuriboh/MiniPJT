7ed48d1c1da69c349b7fd9c275a10f731703ad7a
bitcamp <bitcamp@bit>
2024-09-25 17:27:14 +0900

    update User Domain

--- OwnModel2MVCShopREST/src/main/java/com/model2/mvc/service/domain/User.java
+++ OwnModel2MVCShopREST/src/main/java/com/model2/mvc/service/domain/User.java
@@ -3,6 +3,7 @@ package com.model2.mvc.service.domain;
 import java.sql.Date;
 
 
+//==>회원정보를 모델링(추상화/캡슐화)한 Bean
 public class User {
 	
 	///Field
@@ -19,6 +20,11 @@ public class User {
 	private String phone1;
 	private String phone2;
 	private String phone3;
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// JSON ==> Domain Object  Binding을 위해 추가된 부분
+	private String regDateString;
+
+	
 	
 	///Constructor
 	public User(){
@@ -82,9 +88,20 @@ public class User {
 	public Date getRegDate() {
 		return regDate;
 	}
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// JSON ==> Domain Object  Binding을 위해 추가된 부분
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
+		
+		if(regDate !=null) {
+			// JSON ==> Domain Object  Binding을 위해 추가된 부분
+			this.setRegDateString( regDate.toString().split("-")[0]
+													+"-"+ regDate.toString().split("-")[1]
+													+ "-" +regDate.toString().split("-")[2] );
+		}
+		
 	}
+	
 	/////////////// EL 적용 위해 추가된 getter Method ///////////
 	public String getPhone1() {
 		return phone1;
@@ -95,10 +112,34 @@ public class User {
 	public String getPhone3() {
 		return phone3;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "UserVO : [userId] "+userId+" [userName] "+userName+" [password] "+password+" [role] "+ role
 			+" [ssn] "+ssn+" [phone] "+phone+" [email] "+email+" [regDate] "+regDate;
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////
+	// JSON ==> Domain Object  Binding을 위해 추가된 부분
+	// POJO 의 중요성
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+	
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+	
+	public void setPhone3(String phone3) {
+		this.phone3 = phone3;
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////
+
+	public String getRegDateString() {
+		return regDateString;
+	}
+
+	public void setRegDateString(String regDateString) {
+		this.regDateString = regDateString;
 	}
 }
